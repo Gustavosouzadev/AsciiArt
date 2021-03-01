3e056df90073bec26da35ae4c7b92c6b8d66434a
Gustavo <Gustavo>
2021-03-01 00:31:07 -0300

    Finalizando Asciiart

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+           string L1 = " ||  ||";
+           string L2 = "\\";
+           string L5 = "//";
+           string L6 = "()";
+           string L3 = "(__)";
+           string L4 = "||    ||";
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(L1);
+            Console.Write(" \\");
+            Console.Write(L2);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(L6);
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(L5); 
+            Console.Write("//");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(L3);
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("\\");
+            Console.WriteLine("\\");
+            Console.WriteLine(L4);
+
+            Console.ResetColor();
+        }
+    }
+}
